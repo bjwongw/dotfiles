035fa9b52709daf8afcf224e222682ab83359238
brendonjwong <bjwong.w@gmail.com>
2018-09-06 20:58:04 -0700

    Add vscode extensions to install script

--- vscode/install.sh
+++ vscode/install.sh
@@ -2,7 +2,23 @@
 #
 # Symlink vscode user settings
 
-SETTINGS_DIR='$HOME/Library/Application\ Support/Code/User'
+CURRENT_DIR="$(dirname "$0")"
+SETTINGS_DIR="$HOME/Library/Application Support/Code/User"
 
-ln -s $(pwd)/settings.json $SETTINGS_DIR/settings.json
-ln -s $(pwd)/keybindings.json $SETTINGS_DIR/keybindings.json
+ln -s "$CURRENT_DIR"/settings.json "$SETTINGS_DIR"/settings.json
+ln -s "$CURRENT_DIR"/keybindings.json "$SETTINGS_DIR"/keybindings.json
+
+EXTENSIONS=(
+    avli.clojure
+    dbaeumer.vscode-eslint
+    HookyQR.beautify
+    mechatroner.rainbow-csv
+    ms-python.python
+    ms-vscode.sublime-keybindings
+    redhat.java
+    vscjava.vscode-maven
+)
+
+for e in "${EXTENSIONS[@]}"; do
+    code --install-extension "$e"
+done
